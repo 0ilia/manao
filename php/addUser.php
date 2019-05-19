226f19c9f77fadec2a09c843e3c6d4241be3f2ee
0ilia <ilia1mail2martinkevi4@gmail.com>
2019-05-19 14:20:20 +0300

    q

--- php/addUser.php
+++ php/addUser.php
@@ -37,8 +37,8 @@ if ((strlen($login)>3)&&(strlen($login)<16)&&
         $xml->user[$count - 1]->addChild('password', $passwordHash);
         $xml->user[$count - 1]->addChild('email', $email);
         $xml->user[$count - 1]->addChild('name', $name);
-        $xml->user[$count - 1]->addChild('date', date("F j, Y, g:i a"));
         $xml->user[$count - 1]->addChild('cookie');
+        $xml->user[$count - 1]->addChild('date', date("F j, Y, g:i a"));
         $xml->user[$count - 1]->addChild('ip',$_SERVER['REMOTE_ADDR']);
         file_put_contents('users.xml', $xml->asXML());
         echo "Вы зарегистрированы";
